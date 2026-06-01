ebc0bcbb0a68b3453aee46d3ac16f7dd105ebbb5
joaopfd <joaopfdans@icloud.com>
2026-06-01 16:30:36 -0300

    Create combinatoria.py

---
+++ docs/combinatoria.py
@@ -0,0 +1,7 @@
+entradas = ["Salada", "Sopa", "Bruschetta", "Carpaccio"]
+pratos = ["Frango", "Peixe", "Bife", "Lasanha", "Risoto", "Hambúrguer"]
+sobremesas = ["Pudim", "Sorvete", "Torta"]
+
+total = len(entradas) * len(pratos) * len(sobremesas)
+
+print(f"Total de combinações: {total}")
